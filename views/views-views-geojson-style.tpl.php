afb395b46dadd4f8715e8cb855fe8bc92556e384
Jeff Schuler <jeff@websubstrate.com>
2011-08-27 16:40:30 -0400

    amidst restructuring to use ideas & code from views_datasource

---
+++ views/views-views-geojson-style.tpl.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file views-views-geojson-style.tpl.php
+ * Default template for the Views GeoJSON style plugin using the simple format
+ *
+ * Variables:
+ * - $view: The View object.
+ * - $rows: Hierachial array of key=>value pairs to convert to GeoJSON
+ * - $options: Array of options for this style 
+ *
+ * @ingroup views_templates
+ */
+
+$jsonp_prefix = $options['jsonp_prefix'];
+
+if ($view->override_path) {
+	// We're inside a live preview where the GeoJSON is pretty-printed.
+	$json = _views_geojson_encode_formatted($rows);
+	if ($jsonp_prefix) $json = "$jsonp_prefix($json)";	
+	print "<code>$json</code>";
+}
+else {
+  $json = json_encode($rows);
+  if ($jsonp_prefix) {
+    $json = "$jsonp_prefix($json)"; 
+  }
+  if ($options['using_views_api_mode']) {
+    // We're in Views API mode.
+    print $json;
+  }
+  else {
+    // We want to send the JSON as a server response so switch the content
+    // type and stop further processing of the page.
+    $content_type = ($options['content_type'] == 'default') ? 'application/json' : $options['content_type'];
+    drupal_set_header("Content-Type: $content_type; charset=utf-8");
+    print $json;
+    //Don't think this is needed in .tpl.php files: module_invoke_all('exit');
+    exit;
+  }
+}
