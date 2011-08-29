f323dc7a591fbc42604220397fa2cc4de4bef688
Jeff Schuler <jeff@websubstrate.com>
2011-08-29 02:56:35 -0400

    essentials of rewrite nearly complete

--- views/views-views-geojson-style.tpl.php
+++ views/views-views-geojson-style.tpl.php
@@ -5,8 +5,8 @@
  * Default template for the Views GeoJSON style plugin using the simple format
  *
  * Variables:
- * - $view: The View object.
- * - $rows: Hierachial array of key=>value pairs to convert to GeoJSON
+ * - $view: The View object
+ * - $features: Features data to be serialized as GeoJSON
  * - $options: Array of options for this style 
  *
  * @ingroup views_templates
@@ -14,14 +14,19 @@
 
 $jsonp_prefix = $options['jsonp_prefix'];
 
+$features_collection = array(
+  'type' => 'FeatureCollection',
+  'features' => $features,
+);
+
 if ($view->override_path) {
 	// We're inside a live preview where the GeoJSON is pretty-printed.
-	$json = _views_geojson_encode_formatted($rows);
+	$json = _views_geojson_encode_formatted($features_collection);
 	if ($jsonp_prefix) $json = "$jsonp_prefix($json)";	
 	print "<code>$json</code>";
 }
 else {
-  $json = json_encode($rows);
+  $json = json_encode($features_collection);
   if ($jsonp_prefix) {
     $json = "$jsonp_prefix($json)"; 
   }
@@ -33,7 +38,7 @@ else {
     // We want to send the JSON as a server response so switch the content
     // type and stop further processing of the page.
     $content_type = ($options['content_type'] == 'default') ? 'application/json' : $options['content_type'];
-    drupal_set_header("Content-Type: $content_type; charset=utf-8");
+    drupal_add_http_header("Content-Type", "$content_type; charset=utf-8");
     print $json;
     //Don't think this is needed in .tpl.php files: module_invoke_all('exit');
     exit;
