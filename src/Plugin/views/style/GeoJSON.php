b413d8ce3b24b93f614b6b0aca863683ad1aa4f3
Ethan Presberg <ethan@colosa.com>
2015-04-24 16:03:15 -0400

    First go at translating init function

--- src/Plugin/views/style/GeoJSON.php
+++ src/Plugin/views/style/GeoJSON.php
@@ -18,18 +18,20 @@ class GeoJSON extends StylePluginBase {
     parent::init($view, $display, $options);
 
     // Search api indexes store the entity metadata in the views data array.
-    if (strpos($view->base_table, 'search_api_index_') === 0) {
-      $views_data = views_fetch_data();
-      if (isset($views_data[$view->base_table]['table']['entity type'])) {
-        $this->entity_type = $views_data[$view->base_table]['table']['entity type'];
-        $this->entity_info = entity_get_info($this->entity_type);
+    if (strpos($view->storage->get('base_table'), 'search_api_index_') === 0) {
+      //$views_data = views_fetch_data();
+      $views_data = views_views_data();
+      if (isset($views_data[$view->storage->get('base_table')]['table']['entity type'])) {
+        $this->entity_type = $views_data[$view->storage->get('base_table')]['table']['entity type'];
+        //$this->entity_info = entity_get_info($this->entity_type);
+        $this->entity_info = \Drupal::entityManager()->getDefinitions($this->entity_type);
       }
     }
     else {
       // Pretty-print the JSON.
       module_load_include('inc', 'views_geojson', 'views_geojson.helpers');
-      foreach (entity_get_info() as $key => $info) {
-        if ($view->base_table == $info['base table']) {
+      foreach (\Drupal::entityManager()->getDefinitions() as $key => $info) {
+        if ($view->storage->get('base_table') == $info['base table']) {
           $this->entity_type = $key;
           $this->entity_info = $info;
           break;
