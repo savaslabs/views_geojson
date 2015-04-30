9771837c29678544e3a341a2cd7f41ec09bbb89e
Anne Tomasevich <annetvich@gmail.com>
2015-04-30 17:43:35 -0400

    Issue #28: Customize views geoJSON plugin a bit more
    Still not showing up in "Add new view."

--- src/Plugin/views/wizard/ViewsGeoJsonWizard.php
+++ src/Plugin/views/wizard/ViewsGeoJsonWizard.php
@@ -17,42 +17,44 @@ use Drupal\Core\Form\FormStateInterface;
 class ViewsGeoJsonWizard extends WizardPluginBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
 
-    $form['displays']['views_geojson_export'] = array(
+    $form['displays']['views_geojson'] = array(
       '#type' => 'fieldset',
-      '#title' => $this->t('Views GeoJSON export settings'),
+      '#title' => $this->t('Views GeoJSON settings'),
       '#attributes' => array('class' => array('views-attachment', 'fieldset-no-legend')),
       '#tree' => TRUE,
     );
     $form['displays']['views_geojson_export']['create'] = array(
-      '#title' => $this->t('Provide a REST export'),
+      '#title' => $this->t('Create a geoJSON view'),
       '#type' => 'checkbox',
       '#attributes' => array('class' => array('strong')),
-      '#id' => 'edit-rest-export-create',
+      '#id' => 'edit-views-geojson-create',
     );
 
-    // All options for the REST export display are included in this container
-    // so they can be hidden as a group when the "Provide a REST export"
+    // All options for the views geoJSON display are included in this container
+    // so they can be hidden as a group when the "Create a geoJSON view"
     // checkbox is unchecked.
-    $form['displays']['views_geojson_export']['options'] = array(
+    $form['displays']['views_geojson']['options'] = array(
       '#type' => 'container',
       '#attributes' => array('class' => array('options-set')),
       '#states' => array(
         'visible' => array(
-          ':input[name="views_geojson_export[create]"]' => array('checked' => TRUE),
+          ':input[name="views_geojson[create]"]' => array('checked' => TRUE),
         ),
       ),
-      '#prefix' => '<div id="edit-rest-export-wrapper">',
+      '#prefix' => '<div id="edit-views-geojson-wrapper">',
       '#suffix' => '</div>',
-      '#parents' => array('views_geojson_export'),
+      '#parents' => array('views_geojson'),
     );
 
-    $form['displays']['views_geojson_export']['options']['path'] = array(
-      '#title' => $this->t('REST export path'),
-      '#type' => 'textfield',
-      '#field_prefix' => $path_prefix,
-      // Account for the leading backslash.
-      '#maxlength' => 254,
-    );
+//    $form['displays']['views_geojson']['options']['path'] = array(
+//      '#title' => $this->t('Views GeoJSON path'),
+//      '#type' => 'textfield',
+//      '#field_prefix' => $path_prefix,
+//      // Account for the leading backslash.
+//      '#maxlength' => 254,
+//    );
+
+
     return $form;
   }
 }
